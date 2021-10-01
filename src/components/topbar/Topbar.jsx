079529f07395805fdff8f6a3264a4d76d72568b4
Matthew Lopez <mlopez@domain.local>
2021-10-01 14:55:18 -0400

    starting to build new portfolio

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import './topbar.scss'
+
+export default function Topbar() {
+    return (
+        <div className='topbar'>
+            <div className='wrapper'>
+                <div className="left">
+                    <a href="#intro">dfaaf</a>
+                </div>
+                <h1>Hello</h1>
+                <div className="right">
+                    This is right side
+                </div>
+            </div>
+            
+        </div>
+    )
+}
