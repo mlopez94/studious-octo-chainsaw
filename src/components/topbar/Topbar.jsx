283c0df30daad9f700d312a8f128e842b658a485
Matthew Lopez <mlopez@domain.local>
2021-10-01 16:54:11 -0400

    updating topbar functionality

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,33 @@
 import './topbar.scss'
+import { Person, Mail } from '@material-ui/icons'
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className='topbar'>
+        <div className={'topbar ' + (menuOpen && "active")}>
             <div className='wrapper'>
                 <div className="left">
-                    <a href="#intro">dfaaf</a>
+                    <a href="#intro" className='logo'>Matt Lopez</a>
+                    <div className="itemContainer">
+                        <Person className='icon' />
+                        <span>201 994 9107</span>
+
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className='icon' />
+                        <span>lopezmatthew87@gmail.com</span>
+
+                    </div>
+
                 </div>
-                <h1>Hello</h1>
                 <div className="right">
-                    This is right side
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen) }>
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }
